3138e1f028cf7df9239b5a2d6aa0ea79dec0ea04
eminezubeyde <113181394+eminezubeyde@users.noreply.github.com>
2023-01-29 17:31:53 +0300

    add

---
+++ e-commerce/src/main/java/org/example/model/Customer.java
@@ -0,0 +1,12 @@
+package org.example.model;
+
+import java.util.List;
+
+public class Customer {
+    private long id;
+    private String name;
+    private String username;
+    private String password;
+    private Address address;
+    private List<Order> orderList;
+}
